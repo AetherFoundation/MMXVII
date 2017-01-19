f604ebae8d0c5e42dd96877a2ec2bf4e542f3f32
riku <riku@Luka>
2017-01-19 04:06:46 -0500

    minor updates

--- MMXVII/src/moe/thisis/mmxvii/engine/graph/Mesh.java
+++ MMXVII/src/moe/thisis/mmxvii/engine/graph/Mesh.java
@@ -1,8 +1,4 @@
-/**
- * 
- */
 package moe.thisis.mmxvii.engine.graph;
-
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -11,23 +7,18 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
-/**
- * @author riku
- *
- */
+
 public class Mesh {
 
     private final int vaoId;
-    
-    private final int posVboId;
 
-    private final int colourVboId;
+    private final int posVboId;
 
     private final int idxVboId;
 
     private final int vertexCount;
-    
-    public Mesh(float[] positions, float[] colours, int[] indices) {
+
+    public Mesh(float[] positions, int[] indices) {
         vertexCount = indices.length;
 
         vaoId = glGenVertexArrays();
@@ -40,14 +31,6 @@ public class Mesh {
         glBindBuffer(GL_ARRAY_BUFFER, posVboId);
         glBufferData(GL_ARRAY_BUFFER, posBuffer, GL_STATIC_DRAW);
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-        
-        // Colour VBO
-        colourVboId = glGenBuffers();
-        FloatBuffer colourBuffer = BufferUtils.createFloatBuffer(colours.length);
-        colourBuffer.put(colours).flip();
-        glBindBuffer(GL_ARRAY_BUFFER, colourVboId);
-        glBufferData(GL_ARRAY_BUFFER, colourBuffer, GL_STATIC_DRAW);
-        glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
 
         // Index VBO
         idxVboId = glGenBuffers();
@@ -59,7 +42,7 @@ public class Mesh {
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
     }
-    
+
     public int getVaoId() {
         return vaoId;
     }
@@ -74,7 +57,6 @@ public class Mesh {
         // Delete the VBOs
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glDeleteBuffers(posVboId);
-        glDeleteBuffers(colourVboId);
         glDeleteBuffers(idxVboId);
 
         // Delete the VAO
--- MMXVII/src/moe/thisis/mmxvii/game/DummyGame.java
+++ MMXVII/src/moe/thisis/mmxvii/game/DummyGame.java
@@ -2,32 +2,42 @@ package moe.thisis.mmxvii.game;
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_DOWN;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_UP;
-import static org.lwjgl.opengl.GL11.glViewport;
 import moe.thisis.mmxvii.engine.IGameLogic;
 import moe.thisis.mmxvii.engine.Window;
+import moe.thisis.mmxvii.engine.graph.Mesh;
 
 public class DummyGame implements IGameLogic {
 
     private int direction = 0;
-    
+
     private float color = 0.0f;
 
     private final Renderer renderer;
-    
+
+    private Mesh mesh;
+
     public DummyGame() {
         renderer = new Renderer();
     }
-    
+
     @Override
     public void init() throws Exception {
         renderer.init();
+        float[] positions = new float[]{
+            -0.5f, 0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.5f, 0.5f, 0.0f,};
+        int[] indices = new int[]{
+            0, 1, 3, 3, 1, 2,};
+        mesh = new Mesh(positions, indices);
     }
-    
+
     @Override
     public void input(Window window) {
-        if ( window.isKeyPressed(GLFW_KEY_UP) ) {
+        if (window.isKeyPressed(GLFW_KEY_UP)) {
             direction = 1;
-        } else if ( window.isKeyPressed(GLFW_KEY_DOWN) ) {
+        } else if (window.isKeyPressed(GLFW_KEY_DOWN)) {
             direction = -1;
         } else {
             direction = 0;
@@ -39,23 +49,21 @@ public class DummyGame implements IGameLogic {
         color += direction * 0.01f;
         if (color > 1) {
             color = 1.0f;
-        } else if ( color < 0 ) {
+        } else if (color < 0) {
             color = 0.0f;
         }
     }
 
     @Override
     public void render(Window window) {
-        if ( window.isResized() ) {
-            glViewport(0, 0, window.getWidth(), window.getHeight());
-            window.setResized(false);
-        }
-
         window.setClearColor(color, color, color, 0.0f);
-        renderer.clear();
+        renderer.render(window, mesh);
     }
 
-	/* (non-Javadoc)
-	 * @see moe.thisis.mmxvii.engine.IGameLogic#cleanup()
-	 */
+    @Override
+    public void cleanup() {
+        renderer.cleanup();
+        mesh.cleanUp();
+    }
+
 }
